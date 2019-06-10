d7ed9ab1eec17aee3f6f98d63d0ecf551df8cd4f
Artem <inste@icloud.com>
2019-06-10 21:51:32 +0300

    Second commit

---
+++ SwiftBookAppSwiftUI/TopView.swift
@@ -0,0 +1,30 @@
+//
+//  TopView.swift
+//  SwiftBookAppSwiftUI
+//
+//  Created by Artem Karmaz on 09.06.2019.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopView : View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("info@cocoa.jumbo")
+                Image("like")
+                Text("12.2k")
+            }
+            Text("Test text")
+        }
+    }
+}
+
+#if DEBUG
+struct TopView_Previews : PreviewProvider {
+    static var previews: some View {
+        TopView()
+    }
+}
+#endif
